e92455b09f3c2e4928f2522bb71d2e92c3d547d1
Luis Alvarez <alvarez.luis20@gmail.com>
2024-04-01 13:06:07 -0500

    Mostrando los productos

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { ProductComponent } from '../../components/product/product.component';
+
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
