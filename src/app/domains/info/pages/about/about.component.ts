2249a6b71340ad6bf3710a940e03f07709529625
Luis Alvarez <alvarez.luis20@gmail.com>
2024-04-01 17:48:30 -0500

    ngOnInit. ngOnAfterViewInit. ngOnDestroy

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { CounterComponent } from '../../../shared/components/counter/counter.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CounterComponent],
+  imports: [CommonModule, CounterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
@@ -24,5 +25,28 @@ export class CounterComponent {
     console.log('ngOnChanges');
     console.log('-'.repeat(15));
     console.log(changes);
+  }
+
+  ngOnInit(){
+    //after render
+    // Solo se ejecuta 1 vez
+    // Se puede ejecutar funcione async (promise, then, subcribe)
+    console.log('ngOnInit');
+    console.log('-'.repeat(15));
+    console.log('duration => '+this.duration);
+    console.log('message => '+this.message);
+  }
+
+  ngAfterViewInit(){
+    //after render
+    // Solo se ejecuta 1 vez Luego renderizar los hijos del componente
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(15));
+  }
+
+  ngOnDestroy(){
+    //Al eliminar el componente
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(15));
   }
 }
