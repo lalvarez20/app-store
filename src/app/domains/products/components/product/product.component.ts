6a335a36780a9ff8bebec45afe63f3031a208e33
Luis Alvarez <alvarez.luis20@gmail.com>
2024-04-02 12:29:35 -0500

    crear Interface product. Mejorar vista producto con un solo Input

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -9,13 +10,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ProductComponent {
   //img = 'https://picsum.photos/640/640?r=' + Math.random()
-  @Input({ required:true }) img: string = '';
-  @Input({ required:true }) title: string = '';
-  @Input({ required:true }) price: number = 0;
+  @Input({ required:true }) product!: Product;
 
   @Output() addToCard = new EventEmitter();
 
   addToCartHandler(){
-    this.addToCard.emit('Mensaje del hijo ' + this.title);
+    this.addToCard.emit('Mensaje del hijo ' + this.product.title);
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from './../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor(){
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Prod 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=11',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Prod 2',
+        price: 120,
+        image: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Prod 3',
+        price: 135,
+        image: 'https://picsum.photos/640/640?r=13',
+        creationAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProducts);
+  }
 
   fronClidEvent(event: string){
     console.log('From Dad: ' + event)
