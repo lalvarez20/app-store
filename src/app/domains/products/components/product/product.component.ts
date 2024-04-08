e4166eb94e4b03a217559efe76f547d3bf5f0ef8
Luis Alvarez <alvarez.luis20@gmail.com>
2024-04-08 18:23:03 -0500

    crear componente Detalle de producto. Routing con parametros

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,12 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CurrencyPipe, DecimalPipe, DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Product } from '@shared/models/product.model';
 import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CurrencyPipe, DecimalPipe, DatePipe, TimeAgoPipe],
+  imports: [CurrencyPipe, DecimalPipe, DatePipe, TimeAgoPipe, RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
