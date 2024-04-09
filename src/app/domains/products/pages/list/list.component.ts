dc342091e88420ef7e3ca37fb7674144d01cce69
Luis Alvarez <alvarez.luis20@gmail.com>
2024-04-09 18:38:05 -0500

    Mostrar categorias desde el API. Crear category service

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from '@shared/components/header/header.component';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
@@ -17,10 +19,22 @@ import { ProductService } from '@shared/services/product.service';
 export class ListComponent {
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
+
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   ngOnInit(){
+    this.getProducts();
+    this.getCategories();
+  }
+
+  addToCart(newProduct: Product){
+    this.cartService.addToCart(newProduct);
+  }
+
+  private getProducts(){
     this.productService.getProducts()
     .subscribe({
       next: (products) => {
@@ -32,8 +46,16 @@ export class ListComponent {
     })
   }
 
-  addToCart(newProduct: Product){
-    this.cartService.addToCart(newProduct);
+  private getCategories(){
+    this.categoryService.getAllCategories()
+    .subscribe({
+      next: (data) => {
+          this.categories.set(data);
+      },
+      error: () => {
+
+      }
+    })
   }
 
 }
